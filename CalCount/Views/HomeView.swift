247118d7705c44ff04b84b5f3ffa984f1d14f0d0
Brandon Mok <bxm5989@rit.edu>
2020-10-25 21:02:37 -0400

    Stubbed in tabs for tab navigation once logged in

--- CalCount/Views/HomeView.swift
+++ CalCount/Views/HomeView.swift
@@ -1,17 +1,54 @@
-//
-//  HomeView.swift
-//  CalCount
-//
-//  Created by XCodeClub on 2020-10-25.
-//
 
 import SwiftUI
 
-struct HomeView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+// enum for tab navigation
+enum Tabs: Hashable {
+    case home
+    case water
+    case nutrition
+    case settings
 }
+
+struct HomeView: View {
+    @State private var selectedTab = 0
+    
+    var body: some View {
+        VStack {
+            TabView(selection: $selectedTab) {
+                
+                // TEMP - REplace with view instead of Text!
+                Text("Tab Content 1")
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("Home")
+                    }
+                    .tag(Tabs.home)
+                
+                Text("Tab stuff")
+                    .tabItem {
+                        Image(systemName: "drop")
+                        Text("Water")
+                    }
+                    .tag(Tabs.water)
+                
+                Text("Tab again")
+                    .tabItem {
+                        Image(systemName: "doc.on.clipboard")
+                        Text("Nutrition")
+                    }
+                    .tag(Tabs.nutrition)
+
+                
+                Text("Tab Content 2")
+                    .tabItem {
+                        Image(systemName: "gearshape")
+                        Text("Settings")
+                    }
+                    .tag(Tabs.settings)
+            }//tabview
+        }//vstack
+    }//body
+}//struct
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
